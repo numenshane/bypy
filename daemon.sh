23de3a8779a28372041d1bbc38b41ffabee37872
numenshane <numenshane@gmail.com>
2015-12-17 14:04:54 +0800

    Update daemon.sh

--- daemon.sh
+++ daemon.sh
@@ -9,7 +9,7 @@ if  [[ `ps -elf|grep bypy|grep syncup|wc -l` -eq 0 ]]; then
     $pythonExec $execDir/bypy.py syncup $dataDir >> $logFile 2>&1
     if [[ $? -eq 0 ]]; then
         echo "syncup down, clean and recycle local dir" >> $logFile
-        rm -fr /dataDir/*
+        rm -fr $dataDir/*
     else
         echo "Err: return status: $?" >> $logFile
     fi
