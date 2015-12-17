894d2f687544953d822e901602a63e51ad786d53
numenshane <numenshane@gmail.com>
2015-12-17 13:07:14 +0800

    Create daemon.sh
    add this scriptes to crontab, remote sync add files to my baiduyun clouddisk
    install crond by: 
         yum install cronie -y
    edit crontab files by: 
        crontab -e
                */5 * * * * ( /Data/bypy/daemon.sh > /dev/null 2>&1 )
    following testing cross region upload rates

---
+++ daemon.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+execDir=/Data/bypy # bypy程序目录
+logFile=$execdir/bypy.log # 日志文件
+dataDir=/Data/youtube # 要远程同步上传至数据文件夹
+pythonExec=/usr/local/bin/python2.7 # python binary (version > 2.7)
+
+if  [[ `ps -elf|grep bypy|grep syncup|wc -l` -eq 0 ]]; then  
+    echo `date` > $logFile
+    $pythonExec $execDir/bypy.py syncup $dataDir >> $logFile 2>&1
+    if [[ $? -eq 0 ]]; then
+        echo "syncup down, clean and recycle local dir" >> $logFile
+        rm -fr /dataDir/*
+    else
+        echo "Err: return status: $?" >> $logFile
+    fi
+else
+    echo "bypy currently is running $(date)!" >> $logFile
+fi
