e521818ca8bc82b88feea7b76fb487e6448539a9
numenshane <numenshane@gmail.com>
2016-01-14 10:37:52 +0800

    modify logs

--- daemon.sh
+++ daemon.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 execDir=/Data/bypy # bypy程序目录
-logFile=$execDir/bypy.log # 日志文件
+# logFile=$execDir/bypy.internal.log # 日志文件
 dataDir=/Data/youtube # 要远程同步上传至数据文件夹
 pythonExec=/usr/local/bin/python2.7 # python binary (version > 2.7)
 
+echo +++ $0 `date` this round started
 if  [[ `ps -elf|grep bypy|grep syncup|wc -l` -eq 0 ]]; then  
-    echo `date` > $logFile
     cd $dataDir
     #recording the current file numbers
     lines_bef=`ls -l|wc -l`
-    $pythonExec $execDir/bypy.py syncup $dataDir >> $logFile 2>&1
+    $pythonExec $execDir/bypy.py syncup $dataDir 
     if [[ $? -eq 0 ]]; then
 	lines_cur=`ls -l|wc -l`
 	if [ $lines_bef -eq $lines_cur ]; then 
-            echo "syncup down, clean and recycle local dir" >> $logFile
+            echo "`date` syncup down, clean and recycle local dir"
             rm -fr $dataDir/*
 	else
 	    echo "$(date) adding more files during sync time, so don't clean $dataDir right now, delay to next time"	
 	fi
     else
-        echo "$(date) Err: return status: $?" >> $logFile
+        echo "$(date) Err: return status: $?"
     fi
 else
-    echo "$(date) bypy currently is running !" >> $logFile
+    echo "$(date) bypy currently is running !"
 fi
+echo --- $0 `date` this round ended
