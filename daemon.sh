5811c7751889d1de168715c9fa20949dc68c9e65
numenshane <numenshane@gmail.com>
2016-02-09 21:54:42 +0800

    ...

--- daemon.sh
+++ daemon.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-execDir=/Data/bypy # bypy程序目录
-# logFile=$execDir/bypy.internal.log # 日志文件
+execDir=/root/bypy # bypy程序目录
 dataDir=/Data/youtube # 要远程同步上传至数据文件夹
 pythonExec=/usr/local/bin/python2.7 # python binary (version > 2.7)
 
@@ -13,7 +12,9 @@ if  [[ `ps -elf|grep bypy|grep syncup|wc -l` -eq 0 ]]; then
     if [[ $? -eq 0 ]]; then
 	lines_cur=`ls -l|wc -l`
 	if [ $lines_bef -eq $lines_cur ]; then 
-            echo "`date` syncup down, clean and recycle local dir"
+            echo "`date` syncup completed"
+	    echo "remove files below"
+	    echo "find . -type f"
             rm -fr $dataDir/*
 	else
 	    echo "$(date) adding more files during sync time, so don't clean $dataDir right now, delay to next time"	
