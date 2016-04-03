ff889d78d54f71df834813102faeefd277deb1cf
numenshane <numenshane@gmail.com>
2016-04-03 13:45:52 +0800

    ...

--- daemon.sh
+++ daemon.sh
@@ -7,9 +7,9 @@ echo +++ $0 `date` this round started
 if  [[ `ps -elf|grep "$execDir/bypy.py" |grep upload|wc -l` -eq 0 ]]; then  
     cd $dataDir
     #recording the current file numbers
-    find . -name * -exec bash -c '/usr/bin/rename_pl "s/ |:|,|&|;|\(|\)|\[|\]|\\\//g" "$1"' -- {} \;
-    find . -maxdepth 1 -type f -exec bash -c 'if [ "$1" != "." ]; then /Data/bypy/bypy.py upload "$1" && rm -fr "$1"; fi' -- {} \;  
-    find . -maxdepth 1 -type d -exec bash -c 'if [ "$1" != "." ]; then /Data/bypy/bypy.py upload "$1" `date +%Y-%m-%d` && rm -fr "$1"; fi' -- {} \;  
+    find . -name "*" -exec bash -c '/usr/bin/rename_pl "s/ |:|,|&|;|\(|\)|\[|\]|\\\//g" "$1"' -- {} \;
+    find . -maxdepth 1 -type f -exec bash -c 'if [ "$1" != "." ]; then /usr/local/bin/python2.7 /Data/bypy/bypy.py upload "$1" && rm -fr "$1"; fi' -- {} \;  
+    find . -maxdepth 1 -type d -exec bash -c 'if [ "$1" != "." ]; then /usr/local/bin/python2.7 /Data/bypy/bypy.py upload "$1" `date +%Y-%m-%d` && rm -fr "$1"; fi' -- {} \;  
 
     #lines_bef=`ls -l|wc -l`
     #$pythonExec $execDir/bypy.py syncup $dataDir 
