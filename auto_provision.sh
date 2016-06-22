ff2f1bd3f77d5a565c58eeb4ca1b020bf10772ce
root <root@numenshane-hk.(none)>
2016-06-22 16:34:15 +0800

    modify install

--- auto_provision.sh
+++ auto_provision.sh
@@ -12,9 +12,11 @@ fi
 echo "++++++ installing easy_install ..."
 tmp_dir="./tmp`date +%y%m%d%H%M%s`"
 mkdir -p ${tmp_dir} && cd ${tmp_dir}
+# below cause error 
 wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py
 python2.7 ez_setup.py
 cd .. && rm -fr ${tmp_dir}
+
 easy_install-2.7 pip
 # install requests
 easy_install-2.7 requests
@@ -24,7 +26,7 @@ echo "++++++ now auth this python client of bypy...."
 python2.7 bypy.py list
 
 # adding to crontab like this
-echo " */2 * * * * ( /Data/bypy/daemon.sh >> /Data/bypy/bypy.log 2>&1 )"
+echo " */2 * * * * ( /Data/bypy/daemon.sh /Data/youtube >> /Data/bypy/bypy.log 2>&1 )"
 
 exit 0
 
