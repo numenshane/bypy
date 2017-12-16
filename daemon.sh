b0fc84121136aefaea3531e9d24b11d9690ba8e8
numenshane <numenshane@gmail.com>
2017-12-16 11:43:34 +0800

    adding check remote existance before locally clean

--- daemon.sh
+++ daemon.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
-execDir=/Data/bypy # bypy程序目录
 if [ $# -eq 0 ]; then 
     echo "please input local sync dir"
     exit; 
 fi
+
+function safely_clean {
+	bypy search `echo "$1"|sed 's/^\.\///'`|grep -i 'Nothing found' >> /dev/null
+	if [ $? -ne 0 ]; then 
+		rm -fr "$1";
+		echo "$(date) safely remove $1"
+	fi
+}
+
 mkdir -p $1
-dataDir=$1 # 要远程同步上传至数据文件夹
+dataDir=$1 #local sync data path
 pythonExec=/usr/bin/python2.7 # python binary (version > 2.7)
 
 echo +++ $0 `date` this round started
@@ -14,9 +22,10 @@ pidFile=/var/bypy/`echo "$1" | sed 's/\//_/g' | sed 's/ //g'`.pid
 if [ ! -e "$pidFile" ]; then  
     echo $$ > "$pidFile"
     cd $dataDir
-    find . -maxdepth 1 -type f -exec bash -c 'if [ "$1" != "." ]; then /usr/local/bin/bypy upload "$1"; rm -fr "$1"; fi' -- {} \;  
+    find . -maxdepth 1 -type f -exec bash -c 'if [ "$1" != "." ]; then bypy upload "$1"; fi' -- {} \; -exec \
+	bash -c 'bypy search $(echo "$1"|sed "s/^\.\///")|grep -vi "Nothing found" >> /dev/null && rm -fr "$1" && echo $(date) safely remove "$1" ' -- {} \;
     # ---pending, imp upload local dir to remote dir which is not named by `date +%Y-%m-%d` 
-    find . -maxdepth 1 -type d -exec bash -c 'if [ "$1" != "." ]; then /usr/local/bin/bypy upload "$1" `date +%Y-%m-%d` && rm -fr "$1"; fi' -- {} \;  
+    find . -maxdepth 1 -type d -exec bash -c 'if [ "$1" != "." ]; then bypy upload "$1" `date +%Y-%m-%d` && rm -fr "$1"; fi' -- {} \;  
     rm "$pidFile"
 else
     ps -elf|grep bypy|grep `cat $pidFile` >> /dev/null
