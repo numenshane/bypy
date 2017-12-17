9c6bf6a34aeba041fb10ee7a3e3f60adb5008b57
numenshane <numenshane@gmail.com>
2017-12-17 16:24:26 +0800

    ...

--- health_check.sh
+++ health_check.sh
@@ -2,7 +2,6 @@
 export PATH="/sbin/:/bin:/usr/sbin/:/usr/bin:/usr/local/bin"
 export TERM=xterm
 flow=`/bin/bash /Data/bypy/network-analysis.sh eth0 |grep 'eth0 Transmit'|awk -F" " '{print $3}'|sed -n 's/Kb\/s//p'|sed -n 's/\.[0-9]*//p'`
-echo $flow
 if [[ $flow -lt 100 ]]; then
 	echo "`date` eth0 10s average networkflow:${flow}Kb 太小 -> 外部对接的上传服务（如百度云）对该实例端口限流了 -> 重启实例 使用随机端口"
 	pkill bypy
