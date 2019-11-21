c423431c84e859862cd53ab7a6d5d5671a45b8e4
池田 <numenshane@gmail.com>
2019-11-21 12:10:50 +0800

    Update daemon.sh

--- daemon.sh
+++ daemon.sh
@@ -6,7 +6,7 @@ if [ $# -eq 0 ]; then
 fi
 
 function safely_clean {
-	/usr/local/bin/bypy search `echo "$1"|sed 's/^\.\///'`|grep -i 'Nothing found' >> /dev/null
+	bypy search `echo "$1"|sed 's/^\.\///'`|grep -i 'Nothing found' >> /dev/null
 	if [ $? -ne 0 ]; then 
 		rm -fr "$1";
 		echo "$(date) safely remove $1"
@@ -24,8 +24,8 @@ pidFile=/var/bypy/`echo "$1" | sed 's/\//_/g' | sed 's/ //g'`.pid
 if [ ! -e "$pidFile" ]; then  
     echo $$ > "$pidFile"
     cd $dataDir
-    find . -maxdepth 1 -type f -exec bash -c 'if [ "$1" != "." ]; then /usr/local/bin/bypy upload "$1"; fi' -- {} \; -exec \
-	bash -c 'f=$(echo "$1"|sed "s/^\.\///"); /usr/local/bin/bypy search "$f"|grep -vi "Nothing found" >> /dev/null && rm -fr "$1" && echo $(date) safely remove "$1" ' -- {} \;
+    find . -maxdepth 1 -type f -exec bash -c 'if [ "$1" != "." ]; then bypy upload "$1"; fi' -- {} \; -exec \
+	bash -c 'f=$(echo "$1"|sed "s/^\.\///"); bypy search "$f"|grep -vi "Nothing found" >> /dev/null && rm -fr "$1" && echo $(date) safely remove "$1" ' -- {} \;
     # ---pending, imp upload local dir to remote dir which is not named by `date +%Y-%m-%d` 
     find . -maxdepth 1 -type d -exec bash -c 'if [ "$1" != "." ]; then bypy upload "$1" `date +%Y-%m-%d` && rm -fr "$1"; fi' -- {} \;  
     rm "$pidFile"
